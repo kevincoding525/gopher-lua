e5fff095b1ae7b3895d1f080ca7ebd23be8dca60
kevin ouyang <kevinouyang25@gmail.com>
2023-08-19 15:41:13 +0800

    update max array index

--- config.go
+++ config.go
@@ -10,7 +10,7 @@ var RegistrySize = 256 * 20
 var RegistryGrowStep = 32
 var CallStackSize = 256
 var MaxTableGetLoop = 100
-var MaxArrayIndex = 500000
+var MaxArrayIndex = 0
 
 type LNumber float64
 
